1e20b419c0118b63080232a2434b0ac1d6048d86
stavr0s8 <nickdome@gmail.com>
2014-10-20 19:18:06 -0700

    fdasf

--- assets/Game.py
+++ assets/Game.py
@@ -1,13 +1,13 @@
 class Descent_Game():
     """The overall initial game state"""
-    def __init__(self, many, overlordYN):
+    def __init__(self):
         self.game_ID = ""
-        self.players = many
+        self.players = 0
         self.who = 0
         self.lives = 0
-        self.overlord = overlordYN
+        self.overlord = true
         self.SKELETONS = []
         self.SORCERORS = []
 
-        for i in range(0,self.players):
+        
             
--- descent.py
+++ descent.py
@@ -3,14 +3,11 @@
 from assets.Game import Descent_Game
 
 def main():
-    players = int(raw_input("How many players?"))
     
-
-    overlord = "n"
-    while(overlord.upper() != "Y"):
-        overlord = raw_input("Will you be supplying a human overlord?")
-
-    game = Descent_Game(players,overlord)    
+    game = Descent_Game()
+    game.players = int(raw_input("How many players?"))
+    
+    
 
 if __name__ == "__main__":
     main()
