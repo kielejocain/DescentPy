a1ea106fd1ffaa4472c4e1b8e55c83e2326c12f9
kielejocain <joecken@gmail.com>
2014-10-20 20:01:50 -0700

    enemy tracking

--- assets/Game.py
+++ assets/Game.py
@@ -1,13 +1,22 @@
 class Descent_Game():
     """The overall initial game state"""
     def __init__(self):
-        self.game_ID = ""
-        self.players = 0
-        self.who = 0
-        self.lives = 0
-        self.overlord = true
-        self.SKELETONS = []
-        self.SORCERORS = []
+        self.game_ID = ""       # game instance name (for user connection)
+        self.game_pass = ""     # game instance password
+        self.players = 0        # number of human players
+        self.who = 0            # player whose turn it is
+        self.lives = 0          # number of life tokens to start with
+        self.overlord = True    # is the overlord a human? (for eventual AI)
 
-        
-            
+        # the following track the number of available monsters of each type
+        self.BEASTMAN = {'normal': 0, 'master': 0}
+        self.RAZORWING = {'normal': 0, 'master': 0}
+        self.SKELETON = {'normal': 0, 'master': 0}
+        self.OGRE = {'normal': 0, 'master': 0}
+        self.HELLHOUND = {'normal': 0, 'master': 0}
+        self.MANTICORE = {'normal': 0, 'master': 0}
+        self.SORCERER = {'normal': 0, 'master': 0}
+        self.DRAGON = {'normal': 0, 'master': 0}
+        self.NAGA = {'normal': 0, 'master': 0}
+        self.BANESPIDER = {'normal': 0, 'master': 0}
+        self.DEMON = {'normal': 0, 'master': 0}
